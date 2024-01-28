92b732d234ef727f8258b1ed64d299467e0d8ad6
hoangpnh2000 <phannguyenhuyhoang2000@gmail.com>
2024-01-28 21:27:01 +0700

    update

---
+++ js/bai4.js
@@ -0,0 +1,17 @@
+/**
+ * Input
+ *  khachHang,ma,kenh
+ *
+ * Process
+ *
+ *
+ * Output
+ *  Ma,tienCap
+ *
+ *
+ *
+ */
+
+function tinhTienCap() {}
+
+document.getElementById("btn__bai4").onclick = tinhTienCap;
