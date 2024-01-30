a649c802ae509aef89e9a27b525ed7eab49e2afd
hoangpnh2000 <phannguyenhuyhoang2000@gmail.com>
2024-01-30 15:58:54 +0700

    final update

--- js/bai4.js
+++ js/bai4.js
@@ -12,6 +12,27 @@
  *
  */
 
-function tinhTienCap() {}
+function tinhTienCap() {
+    let khachHang = Number(document.getElementById("inputKhachHang").value);
+    let maKH = document.getElementById("inputMaKH").value;
+    let kenh = Number(document.getElementById("inputKenh").value);
+    let ketNoi = Number(document.getElementById("inputKetNoi").value);
+    let total = 0;
+    if (khachHang == 0) {
+        total = 4.5 + 20.5 + 7.5 * kenh;
+    } else if (khachHang == 1) {
+        if (ketNoi <= 10) {
+            total = 15 + 75 + 50 * kenh;
+        } else {
+            total = 15 + 75 + 5 * (ketNoi - 10) + 50 * kenh;
+        }
+    }
+
+    document.getElementById(
+        "textChange__bai4"
+    ).innerHTML = `Mã khách hàng: ${maKH} <br> Tiền cáp: ${Intl.NumberFormat(
+        "us-US"
+    ).format(total)} USD`;
+}
 
 document.getElementById("btn__bai4").onclick = tinhTienCap;
